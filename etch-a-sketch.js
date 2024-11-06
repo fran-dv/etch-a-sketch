1f4fa3bf17247ebecfb907f18f55d2db1d41d4e4
fran-dv <171869989+fran-dv@users.noreply.github.com>
2024-11-06 12:45:05 -0300

    Implement sketchpad with color picker
    The sketchpad consists of a square grid of divs with customizable
    dimensions, generated via a JavaScript loop. It includes two modes:
    Pencil Mode, for coloring grid squares, and Eraser Mode, for removing
    color.
    
    A color picker on the left, powered by the iro.js API, allows users to
    select a color to use in Pencil Mode.
    
    Note: The color swatch selectors and input field for creating a new grid
    with a custom size are currently non-functional.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -11,6 +11,7 @@ for (let row = 0; row < gridLength; row++) {
     for (let col = 0; col < gridLength; col++) {
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
+        square.style.backgroundColor = '#fff';
         row.appendChild(square);
     }
     sketchGrid.appendChild(row);
@@ -20,6 +21,7 @@ const squares = document.querySelectorAll('.square');
 const pencilMode = document.querySelector('#pencil-mode');
 const eraserMode = document.querySelector('#eraser-mode');
 const userMenu = document.querySelector('#user-menu');
+const sketch = document.querySelector('#sketch');
 
 pencilMode.addEventListener('click', () => {
     draw = true;
@@ -27,24 +29,57 @@ pencilMode.addEventListener('click', () => {
 
 eraserMode.addEventListener('click', () => {
     draw = false;
-    console.log(draw);
-    const eraserMsg = document.createElement('p');
-    eraserMsg.textContent = 'Click on a point to erase it!';
-    userMenu.appendChild(eraserMsg);
 })
 
 squares.forEach((square) => {
-    console.log(square);
     
     square.addEventListener('mouseenter', () => {
         if (draw) {
-            square.style.backgroundColor = 'brown';
+            if (square.style.backgroundColor === 'rgb(255, 255, 255)') {
+                square.style.backgroundColor = colorPicker.color.hexString;
+            }
         }
     })
 
-    square.addEventListener('click', () => {
+    square.addEventListener('mouseenter', () => {
         if (!draw) {
-            square.style.backgroundColor = 'white';
+            square.style.backgroundColor = '#fff';
         }
     })
 })
+
+
+const colorPicker = new iro.ColorPicker("#wheel", {
+    width: 250,
+    color: "#fff",
+    borderWidth: 1,
+    borderColor: "#0d0d0d",
+    layout: [
+        { 
+          component: iro.ui.Wheel
+        },
+        {
+          component: iro.ui.Slider,
+          options: {
+            sliderType: 'value',
+          }
+        },
+      ]
+});
+
+const swatchGrid = document.querySelector('#swatch-grid');
+
+const [black, white, red, blue, yellow, green, orange, purple, gray, pink, cyan, brown] = 
+    ["#000000", "#FFFFFF", "#FF0000", "#0000FF", "#FFFF00", "#00FF00", "#FFA500", "#800080", "#808080", "#FFC0CB", "#00FFFF", "#A52A2A"];
+
+colors = [black, white, red, blue, yellow, green, orange, purple, gray, pink, cyan, brown];
+
+colors.forEach((color) => {
+    const swatch = document.createElement('div');
+    swatch.setAttribute('class', 'swatch');
+    swatch.style.backgroundColor = color;
+    swatchGrid.appendChild(swatch);
+    console.log(color);
+})
+
+
